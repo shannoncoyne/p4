2ac8575798bf3e3b058543afcc81a7801ec6abaf
Shannon Coyne <scoyne@wellesley.edu>
2014-08-03 21:59:56 -0400

    Add Heroku creds and update default route to hit the DB.

--- app/config/production/database.php
+++ app/config/production/database.php
@@ -18,14 +18,21 @@ return array(
 	|
 	*/
 
+	$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+	
+	$host = $url["host"];
+	$username = $url["user"];
+	$password = $url["pass"];
+	$database = substr($url["path"], 1);
+
 	'connections' => array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => 'localhost',
-			'database'  => 'homestead',
-			'username'  => 'root',
-			'password'  => 'root',
+			'host'      => $host,
+			'database'  => $database,
+			'username'  => $username,
+			'password'  => $password,
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
